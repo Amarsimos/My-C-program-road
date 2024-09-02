1ee48774a717ceaba48608ad475469a7b27084f4
Amarsimos <amarsimoss@gmail.com>
2024-09-02 16:50:28 +0800

    modified:   .vscode/launch.json 	new file:   CPrimerPlus/chapter2/2.c 	modified:   CPrimerPlus/chapter2/3.c

--- CPrimerPlus/chapter2/3.c
+++ CPrimerPlus/chapter2/3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int y2d(int year);
-int main() 
+int main()
 {
     int year;
-    scanf("%d",&year);
-    printf("%dyears is %d days.\n",year,y2d(year));
+    scanf("%d", &year);
+    printf("%dyears is %d days.\n", year, y2d(year));
     return 0;
 }
-int y2d(int year) 
+int y2d(int year)
 {
     int day;
     day = year * 365 + day;
